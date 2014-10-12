6e982db2d6b0278215b795ae47e9f3ad3e8e18cc
bolasblack <bolasblack@gmail.com>
2014-10-12 16:06:09 +0800

    chore(): 现在是每两个小时随机选择一个用户发送每日邮件

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@
 job_type :shell_with_env, "cd :path && source .env && bin/rails runner -e :environment \":task\" :output"
 
 every 2.hours do
-  shell_with_env 'DiaryMailer.daily(User.find_by_id 5).deliver', :output => "log/crontab.log", :environment => "development"
+  shell_with_env 'DiaryMailer.daily(User.all.sample).deliver', :output => "log/crontab.log", :environment => "development"
 end
