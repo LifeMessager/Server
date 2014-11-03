7f4575797dbe664093193beaf228355b513edbca
bolasblack <bolasblack@gmail.com>
2014-11-03 11:02:48 +0800

    fix(计划任务): 修复 Mailgun 请求总是失败的问题

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 
 job_type :shell_with_env, "cd :path && source .env && bin/rails runner -e :environment \":task\" :output"
-job_type :rake_with_env , "cd :path && :environment_variable=:environment bin/rake :task --silent :output"
+job_type :rake_with_env , "cd :path && source .env && :environment_variable=:environment bin/rake :task --silent :output"
 
 every 1.hours do
   rake_with_env 'schedule:mail_hourly', :output => "log/crontab.log"
