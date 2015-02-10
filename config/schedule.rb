de6beb0e5f57d0e1e3c56c1a39ef5a987740ea49
bolasblack <bolasblack@gmail.com>
2015-02-10 11:52:20 +0800

    fix(): 修复 Ubuntu 下跑定时任务时找不到 ruby 的 BUG

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,21 @@
+require 'rails'
 
-job_type :shell_with_env, "cd :path && source .env && bin/rails runner -e :environment \":task\" :output"
-job_type :rake_with_env , "cd :path && source .env && :environment_variable=:environment bin/rake :task --silent :output"
+job_type :shell_with_env, <<-SHELL
+  export PATH="$HOME/.rbenv/bin:$PATH" &&
+  eval "$(rbenv init -)" &&
+  cd :path &&
+  source .env &&
+  bin/rails runner -e :environment ":task" :output"
+SHELL
+
+job_type :rake_with_env , <<-SHELL
+  export PATH="$HOME/.rbenv/bin:$PATH" &&
+  eval "$(rbenv init -)" &&
+  source ~/.bashrc &&
+  cd :path &&
+  source .env &&
+  :environment_variable=:environment bin/rake :task --silent :output
+SHELL
 
 every :hour do
   rake_with_env 'schedule:mail_hourly', output: 'log/crontab.log'
@@ -10,13 +25,14 @@ every :day do
   rake_with_env 'schedule:delete_user_daily', output: 'log/crontab.log'
 end
 
-every :day, at: '20:00' do
+time = ActiveSupport::TimeZone['Asia/Shanghai'].parse('03:00').utc
+every :day, at: time.strftime('%H:%M') do
   command_string = <<-SHELL
-  (
-    echo "=================== Start Backup Database ==================";
-    hash backup && backup perform -t backup_db --config-file ../../shared/backup/config.rb;
-    echo "=================== Finish Backup Database =================";
-  )
+    export PATH="$HOME/.rbenv/bin:$PATH" &&
+    eval "$(rbenv init -)" &&
+    echo "=================== Start Backup Database ==================" &&
+    hash backup && backup perform -t backup_db --config-file ../../shared/backup/config.rb &&
+    echo "=================== Finish Backup Database ================="
   SHELL
 
   command command_string, output: 'log/crontab.log'
