47f1fd3e9ae8a4b28a104e14f30faf17efccb55b
bolasblack <bolasblack@gmail.com>
2015-03-03 16:40:30 +0800

    fix(backup): 修复了不会定时备份数据库的 BUG
    之前不会备份是因为 ruby 是 capistrano 通过 rbenv 安装和使用的，使用
    过程中通过 RBENV_VERSION 环境变量来指定 ruby 版本，而不是通过
    `rbenv global version` 命令指定的 ruby 版本，crontab 运行的时候由于
    没有设置 RBENV_VERSION ，导致调用的 ruby 版本不正确，进而无法使用相
    应版本的 gem 安装的 backup 程序

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,15 @@
 require 'rails'
 
-job_type :shell_with_env, <<-SHELL
+preparation = <<-SHELL
   export PATH="$HOME/.rbenv/bin:$PATH" &&
+  export RBENV_ROOT=~/.rbenv &&
+  export RBENV_VERSION=#{RUBY_VERSION} &&
   eval "$(rbenv init -)" &&
-  cd :path &&
-  bin/rails runner -e :environment ":task" :output"
+  cd :path
 SHELL
 
-job_type :rake_with_env , <<-SHELL
-  export PATH="$HOME/.rbenv/bin:$PATH" &&
-  eval "$(rbenv init -)" &&
-  source ~/.bashrc &&
-  cd :path &&
-  :environment_variable=:environment bin/rake :task --silent :output
-SHELL
+job_type :shell_with_env, "#{preparation} && :task :output"
+job_type :rake_with_env , "#{preparation} && :environment_variable=:environment bin/rake :task --silent :output"
 
 every :hour do
   rake_with_env 'schedule:mail_hourly', output: 'log/crontab.log'
@@ -26,12 +22,12 @@ end
 time = ActiveSupport::TimeZone['Asia/Shanghai'].parse('03:00').utc
 every :day, at: time.strftime('%H:%M') do
   command_string = <<-SHELL
-    export PATH="$HOME/.rbenv/bin:$PATH" &&
-    eval "$(rbenv init -)" &&
-    echo "=================== Start Backup Database ==================" &&
-    hash backup && backup perform -t backup_db --config-file ../../shared/backup/config.rb &&
-    echo "=================== Finish Backup Database ================="
+    ~/.rbenv/bin/rbenv exec backup version && {
+      echo "=================== Start Backup Database ==================";
+       ~/.rbenv/bin/rbenv exec backup perform -t backup_db --config-file ../../shared/backup/config.rb;
+      echo "=================== Finish Backup Database =================";
+    }
   SHELL
 
-  command command_string, output: 'log/crontab.log'
+  shell_with_env command_string, output: 'log/crontab.log'
 end
