78714d60a675a16dd6b3173ca5bf562330a6bd43
c4605 <bolasblack@gmail.com>
2015-04-26 01:05:50 +0800

    refactor(models/Settings): 统一使用 Settings.web_url_for 来获取网页版的 url 地址

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,10 +1,39 @@
+class WebUrl
+  def login **params
+    "#{prefix}/login?#{params.to_query}"
+  end
+
+  def change_email **params
+    "#{prefix}/user/email/edit?#{params.to_query}"
+  end
+
+  private
+
+  def url_protocol
+    Rails.env.production? ? 'https' : 'http'
+  end
+
+  def prefix
+    "#{url_protocol}://#{Settings.server_name}/#!"
+  end
+end
+
 class Settings < Settingslogic
   source "#{Rails.root}/config/lifemessager.yml"
   namespace Rails.env
   load! if Rails.env.development?
 
+  def initialize
+    @web_url = WebUrl.new
+    super
+  end
+
   def url_protocol
-    Rails.env.production? ? 'https' : 'http'
+    @web_url.send :url_protocol
+  end
+
+  def web_url_for type, **params
+    @web_url.public_send type.to_sym, **params
   end
 
   def mailer_deliverer_full_address
